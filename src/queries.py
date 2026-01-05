2630b16d0dc4b6857478233b1654084a99a33f41
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2026-01-05 20:35:58 +0100

    Feat: write all dimensions to bigquery (#3)
    * wip
    
    * Implement all loading all dimensions into bigquery

--- src/queries.py
+++ src/queries.py
@@ -9,7 +9,6 @@
     3. most frequent value of stops_per_trip per each route is approx. stops_amount
 """
 
-# todo double check this vibecoded query on some sample data
 LINE_DIM_QUERY = """
 with trip_lengths as (
     select
@@ -58,7 +57,7 @@ route_stops_mode as (
     where rn = 1
 )
 select
-    r.route_id as line_label,
+    r.route_id as id,
     v.carrier as operator,
     case r.route_type
         when 0 then 'tram'
@@ -70,13 +69,18 @@ select
     coalesce(rs.stops_amount, 0) as stops_amount
 from routes r
 left join delays d on r.route_id = d."Route"
-left join vehicle v on d."Vehicle No" = v.vehicle_number
+left join vehicles v on d."Vehicle No" = v.vehicle_number
 left join route_length_mode rl on r.route_id = rl.route_id
 left join route_stops_mode rs on r.route_id = rs.route_id
 """
 
-# todo
 STOP_DIM_QUERY = """
+select
+    stop_id as id,
+    stop_name as name,
+    cast(stop_lat as float) as lat,
+    cast(stop_lon as float) as lon
+from stops
 """
 
 VEHICLE_DIM_QUERY = """
@@ -85,7 +89,7 @@ select
     manufacturer as brand,
     type as v_model,
     cast(production_year as integer) as year_produced
-from vehicle
+from vehicles
 where
     vehicle_number is not null
     and trim(vehicle_number) != ''
@@ -96,4 +100,38 @@ where
     and production_year is not null
     and cast(production_year as varchar) ~ '^\d+$'
 order by vehicle_number
+"""
+
+WEATHER_DIM_QUERY = """
+select
+    id,
+    temperature,
+    fall_mm,
+    fall_type,
+    wind_speed_mps,
+    wind_direction_deg,
+    humidity_percent,
+    pressure_hpa,
+    general_circumstances
+from weather
+"""
+
+TIME_DIM_QUERY = """
+select
+    id,
+    full_timestamp,
+    hour_,
+    weekday,
+    weekday_num,
+    month_,
+    month_num,
+    season,
+    year_,
+    time_of_day,
+    is_business_day
+from time_dim
+"""
+
+DELAY_FACT_QUERY = """
+
 """
